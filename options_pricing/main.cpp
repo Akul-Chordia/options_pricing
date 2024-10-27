ba0ac524376e62242c9bc694d55b0007aaf8fa14
acool8868 <acool8868@gmail.com>
2024-10-27 18:04:24 +0530

    -> changes inputs and included dd/mm/yyyy format

--- options_pricing/main.cpp
+++ options_pricing/main.cpp
@@ -8,34 +8,22 @@
 #include <iostream>
 #include <iomanip>
 #include "black_scholes.h"
+#include "inputs.h"
 
-using namespace std;
 
 int main(int argc, const char * argv[]) {
-    float spot_price;
-    float strike_price;
-    float time_to_expiration;
-    float risk_free_rate;
-    float volatility;
-    float call_price;
-    float put_price;
-    float temp_volatility;
-    float temp_spot_price;
+    float spot_price, strike_price, time_to_expiration, risk_free_rate, volatility;
+    float call_price, put_price;
     while (true){
-        cout << "Enter the spot price (current price of the asset): ";
-        cin >> spot_price;
-        cout << "Enter the strike price (price at which the option can be exercised): ";
-        cin >> strike_price;
-        cout << "Enter the time to expiration (in years, e.g., 0.5 for six months): ";
-        cin >> time_to_expiration;
-        cout << "Enter the risk-free interest rate (in decimal, e.g., 0.05 for 5%): ";
-        cin >> risk_free_rate;
-        cout << "Enter the volatility (standard deviation of the asset's returns, e.g., 0.2 for 20%): ";
-        cin >> volatility;
+        spot_price = safe_cin_float("Enter the spot price (current price of the asset): ");
+        strike_price = safe_cin_float("Enter the strike price (price at which the option can be exercised): ");
+        time_to_expiration = date_in_years("Enter the date of expiration (DD/MM/YYYY): ");
+        risk_free_rate = safe_cin_float("Enter the risk-free interest rate (in decimal, e.g., 0.05 for 5%): ");
+        volatility = safe_cin_float("Enter the volatility (standard deviation of the asset's returns, e.g., 0.2 for 20%): ");
     
         black_scholes(spot_price, strike_price, time_to_expiration, risk_free_rate, volatility, &call_price, &put_price);
-        cout << "Call : $" << call_price << endl;
-        cout << "Put : $" << put_price << endl;
+        std::cout << "Call : $" << call_price << std::endl;
+        std::cout << "Put : $" << put_price << std::endl;
         
         heatmap(spot_price, strike_price, time_to_expiration, risk_free_rate, volatility);
     }
