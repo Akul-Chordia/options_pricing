fb0b01fd8a156088715bda5ba285e991bdbd6bbf
acool8868 <acool8868@gmail.com>
2024-10-27 18:01:13 +0530

    -> error handling in inputs and support for dd/mm/yyyy input

--- options_pricing/inputs.h
+++ options_pricing/inputs.h
@@ -4,6 +4,7 @@
 //
 //  Created by Akul Chordia on 10/27/24.
 //
+#include <ctime>
 
 float safe_cin_float(const std::string& prompt = "") {
     float number;
@@ -26,23 +27,25 @@ float safe_cin_float(const std::string& prompt = "") {
 }
 
 float date_in_years(const std::string& prompt = "") {
-    int dd, mm, yyyy;
+    float number;
+    float dd, mm, yyyy;
     char parser;
-    while (true) {
-        std::cout << prompt;
-        std::cin >> dd >> parser >> mm >> parser >> yyyy;
-
-        if (std::cin.fail() || std::cin.peek() != '\n') {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Invalid input. Please enter a floating-point number.\n";
-        } else {
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            if (number == 69.0f) {
-                exit(69);
-            }
-            return number;
-        }
+    std::cout << prompt;
+    std::cin >> dd >> parser >> mm >> parser >> yyyy;
+    time_t now = time(0);
+    tm int_now = *localtime(&now);
+    yyyy = yyyy - int_now.tm_year - 1900;
+    mm = mm - int_now.tm_mon - 1;
+    if (mm < 0){
+        yyyy--;
+        mm += 12;
     }
+    dd = dd - int_now.tm_mday;
+    if (dd < 0){
+        mm--;
+        dd += 30.5f;
+    }
+    number = yyyy + (mm/12.0f) + (dd/365.25f);
+    return number;
 }
 
